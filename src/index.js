b5df9b945cf2e28f8ed5cc2ee011b41968714dd6
Cristian Rodriguez <cristianrodriguez306024298@gmail.com>
2023-04-17 18:23:13 -0300

    Solucion al 404

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import GifExpertApp from './GifExpertApp';
+
+// import Testeo from './Testeo';
+
+
+import "./index.css"
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <GifExpertApp />
+);
+
+
+
+
+
+
